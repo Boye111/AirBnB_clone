06682cbd0b3db73c71b0b956a247a830044f0f98
Boye <adeboyeakingbelure18@yahoo.com>
2022-08-04 13:55:11 -0700

    AirBnB_clone

---
+++ models/engine/file_storage.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+File Storage Module
+This module is in charge of the storage of the
+classes and their management
+"""
+
+
+import json
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.reviews import Reviews
+from models.state import State
+from models.user import User
+from os import path
+
+
+class FileStorage:
+    """ File Storage Module """
+    __file_path = 'objects.json'
+    __objects = {}
+
+    def all(self):
+        return self.__objects
+
+    def new(self, obj):
+        key = obj.__class__.__name__ + '.' + obj.id
+        self.__objects[key] = obj
+
+    def save(self):
+        json_dict = {}
+        for k, v in self.__objects.items():
+            json_dict[k] = v.to_dict()
+        with open(self.__file_path, mode='w', encoding='utf-8') as f:
+            f.write(json.dumps(json_dict))
+
+    def reload(self):
+        if path.exists(self.__file_path):
+            with open(self.__file_path, mode='r', encoding='utf-8') as f:
+                json_dict = json.loads(f.read())
+                for k, v in json_dict.items():
+                    self.__objects[k] = eval(v['__class__'])(**v)
