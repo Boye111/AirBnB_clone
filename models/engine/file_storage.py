f1108680feed3eeb40f27477c3af27c43372d57d
Boye <adeboyeakingbelure18@yahoo.com>
2022-08-06 12:01:35 -0700

    ./console.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,7 +12,7 @@ from models.amenity import Amenity
 from models.base_model import BaseModel
 from models.city import City
 from models.place import Place
-from models.reviews import Reviews
+from models.review import Review
 from models.state import State
 from models.user import User
 from os import path
