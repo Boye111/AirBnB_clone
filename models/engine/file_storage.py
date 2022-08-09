e822ff61524b126a8a0afbce5c750542aae9ce38
Boye111 <99730834+Boye111@users.noreply.github.com>
2022-08-09 15:34:37 +0100

    AirBnB_clone

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -15,31 +15,37 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-from os import path
 
 
 class FileStorage:
-    """ File Storage Module """
-    __file_path = 'objects.json'
+    """class FileStorage """
+    __file_path = 'file.json'
     __objects = {}
 
     def all(self):
         return self.__objects
 
     def new(self, obj):
-        key = obj.__class__.__name__ + '.' + obj.id
-        self.__objects[key] = obj
+        if obj:
+            key = '{}.{}'.format(type(obj).__name__, obj.id)
+            self.__objects[key] = obj
 
     def save(self):
-        json_dict = {}
-        for k, v in self.__objects.items():
-            json_dict[k] = v.to_dict()
-        with open(self.__file_path, mode='w', encoding='utf-8') as f:
-            f.write(json.dumps(json_dict))
+        obj_dict = {}
+        for key, obj in self.__objects.items():
+            obj_dict[key] = obj.to_dict()
+
+        json_str = json.dumps(obj_dict)
+
+        with open(self.__file_path, 'w', encoding='utf-8') as f:
+            f.write(json_str)
 
     def reload(self):
-        if path.exists(self.__file_path):
-            with open(self.__file_path, mode='r', encoding='utf-8') as f:
-                json_dict = json.loads(f.read())
-                for k, v in json_dict.items():
-                    self.__objects[k] = eval(v['__class__'])(**v)
+        try:
+            with open(self.__file_path, 'r', encoding='utf-8') as f:
+                json_dict = json.load(f)
+                for obj_dict in json_dict.values():
+                    cls = obj_dict['__class__']
+                    self.new(eval('{}({})'.format(cls, '**obj_dict')))
+        except FileNotFoundError:
+            pass
